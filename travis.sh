7072663f7d147312dbaa060ae138347fb4a9b5e9
Marcello Seri <marcello.seri@citrix.com>
2017-03-22 13:40:59 +0000

    travis: use our script instead
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- travis.sh
+++ travis.sh
@@ -3,9 +3,6 @@
 
 set -ex
 
-export OCAML_VERSION=4.02
-export OPAM_INIT=false
-
 get()
 {
   wget "https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/$1"
@@ -21,8 +18,7 @@ upstream()
     packages/upstream               \
     packages/upstream-extra         \
     -maxdepth 1 -mindepth 1 -type d \
-  | awk -F/ '{print $NF}'           \
-  | egrep -v '^(systemd)'
+    | awk -F/ '{print $NF}'
 }
 
 # list of packages -- excluding some that can't be installed on Travis
@@ -30,26 +26,17 @@ xs()
 {
   find \
     packages/xs                     \
+    packages/xs-extra               \
     -maxdepth 1 -mindepth 1 -type d \
-  | awk -F/ '{print $NF}'           \
-  | egrep -v '^(xenctrl|xapi-test-utils)'
+    | awk -F/ '{print $NF}'
 }
 
-# packages from xs-extra/ that we can compile
-XS_EXTRA='
-message-switch
-vncproxy
-wsproxy
-'
-
-# all of our packages that we can compile
-XS_ALL="$(xs) $XS_EXTRA"
-
-opam init -y local file://$PWD
-opam config exec -- opam install -y -j 4 $(upstream) $XS_ALL
+if [ ! -z ${EXTRA_REMOTE+x} ]; then
+    opam remote add extra $EXTRA_REMOTE
+fi
+opam install -y -j 4 $(upstream) $(xs)
 # Workaround to mark failed uninstall as error. We only test
 # the uninstall of the XS_ALL packages but not of the upstream packages.
-opam config exec -- opam remove -y $XS_ALL
-opam config exec -- opam install -y -j 4 $XS_ALL
-
+opam remove -y $(xs)
+opam install -y -j 4 $(xs)
 
