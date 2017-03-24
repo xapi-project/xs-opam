e3de1e7a11a9b56c664bb0e8d757ddc8f11b28ea
Marcello Seri <marcello.seri@citrix.com>
2017-03-24 10:00:35 +0000

    travis: separate non extra packages from extra
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- travis.sh
+++ travis.sh
@@ -3,6 +3,7 @@
 
 set -ex
 
+# override xenctrl.system to have a full-fledged opam repository
 sh  into_repo.sh
 
 get()
@@ -13,25 +14,48 @@ get()
 get .travis-ocaml.sh
 sh  .travis-ocaml.sh
 
-# list of packages -- excluding some that can't be installed on Travis
+# list of upstream packages
 upstream()
 {
   find \
     packages/upstream               \
+    -maxdepth 1 -mindepth 1 -type d \
+    | awk -F/ '{print $NF}'
+}
+
+upstream-extra()
+{
+  find \
     packages/upstream-extra         \
     -maxdepth 1 -mindepth 1 -type d \
     | awk -F/ '{print $NF}'
 }
 
-# list of packages -- excluding some that can't be installed on Travis
+
+# list of xs packages
 xs()
 {
   find \
     packages/xs                     \
+    -maxdepth 1 -mindepth 1 -type d \
+    | awk -F/ '{print $NF}'
+}
+
+xs-extra()
+{
+  find \
     packages/xs-extra               \
     -maxdepth 1 -mindepth 1 -type d \
     | awk -F/ '{print $NF}'
 }
+
+if [ ! -z "${COMPILE_ALL}" ]; then
+    UPSTREAM=$(upstream) $(upstream-extra)
+    XS=$(xs) $(xs-extra)
+else
+    UPSTREAM=$(upstream)
+    XS=$(xs)
+fi
 
 if [ ! -z "${EXTRA_REMOTES}" ]; then
     opam remote add extra "$EXTRA_REMOTES"
@@ -42,10 +66,10 @@ fi
 if [ ! -z "${OPAM_LINT}" ]; then
     find packages -iname opam -print | xargs -n 1 opam lint
 else
-    opam install -y -j 4 $(upstream) $(xs)
+    opam install -y -j 4 $UPSTREAM $XS
     # Workaround to mark failed uninstall as error. We only test
     # the uninstall of the xs packages but not of the upstream packages.
-    opam remove -y $(xs)
-    opam install -y -j 4 $(xs)
+    opam remove -y $XS
+    opam install -y -j 4 $XS
 fi
 
