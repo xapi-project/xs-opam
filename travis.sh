91a2d3a7e10a1f5ae9308bbd6199dcb6ba37ceca
Christian Lindig <christian.lindig@citrix.com>
2017-04-11 17:37:32 +0100

    Further simplify Travis configuration
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- travis.sh
+++ travis.sh
@@ -24,27 +24,10 @@ pkg()
     popd > /dev/null
 }
 
-if [ "${COMPILE_ALL}" = 1 ]; then
-    UPSTREAM="$(pkg upstream upstream-extra)"
-    XS="$(pkg xs xs-extra)"
-else
-    UPSTREAM="$(pkg upstream)"
-    XS="$(pkg xs)"
-fi
+UPSTREAM="$(pkg upstream)"
+XS="$(pkg xs)"
 
-if [ "${EXTRA_REMOTES}" = 1 ]; then
-    opam remote add extra "$EXTRA_REMOTES"
-fi
-
-if [ "${OPAM_LINT}" = 1 ]; then
-    find packages -iname opam -print | xargs -n 1 opam lint
-else
-    opam install -y depext
-    opam depext  -y $UPSTREAM $XS
-    opam install -y -j 4 $UPSTREAM $XS
-    # Workaround to mark failed uninstall as error. We only test
-    # the uninstall of the xs packages but not of the upstream packages.
-    opam remove  -y $XS
-    opam install -y -j 4 $XS
-fi
+opam install -y depext
+opam depext  -y $UPSTREAM $XS
+opam install -y -j 4 $UPSTREAM $XS
 
