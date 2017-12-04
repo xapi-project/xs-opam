58580c5d8bc10729ba9a98255ac53dad61c3c40b
Gabor Igloi <gabor.igloi@citrix.com>
2017-12-04 15:32:10 +0000

    build-docker.sh: simplify using here document
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- build-docker.sh
+++ build-docker.sh
@@ -3,13 +3,15 @@
 
 set -ex
 
-echo FROM ocaml/opam:${DISTRO}_ocaml-${OCAML_VERSION} > Dockerfile.test
-echo COPY . /xs-opam >> Dockerfile.test
-echo "ENV BASE_REMOTE=${BASE_REMOTE}"     >> Dockerfile.test
-echo "ENV EXTRA_REMOTES=${EXTRA_REMOTES}" >> Dockerfile.test
-echo "ENV COMPILE_ALL=${COMPILE_ALL}"     >> Dockerfile.test
-echo "ENV OPAM_LINT=${OPAM_LINT}"         >> Dockerfile.test
-echo "ENV OCAML_VERSION=${OCAML_VERSION}" >> Dockerfile.test
-echo WORKDIR /xs-opam                     >> Dockerfile.test
-echo RUN bash travis.sh                   >> Dockerfile.test
+cat <<EOF > Dockerfile.txt
+FROM ocaml/opam:${DISTRO}_ocaml-${OCAML_VERSION}
+COPY . /xs-opam
+"ENV BASE_REMOTE=${BASE_REMOTE}"
+"ENV EXTRA_REMOTES=${EXTRA_REMOTES}"
+"ENV COMPILE_ALL=${COMPILE_ALL}"
+"ENV OPAM_LINT=${OPAM_LINT}"
+"ENV OCAML_VERSION=${OCAML_VERSION}"
+WORKDIR /xs-opam
+RUN bash travis.sh
+EOF
 docker build -t local-build -f Dockerfile.test .
