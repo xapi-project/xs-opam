f771d752125c64138f832a11a667caf65844b032
Christian Lindig <christian.lindig@citrix.com>
2018-10-02 14:18:52 +0100

    Remove old tools (for now)
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- tools/dev-repos.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-# This emits the URLs of all dev repos mentioned in the opam 
-# files. This relies on the opam-ed tool that can be installed using
-# opam.
-
-find . -name opam -type f | while read opam; do
-  cat $opam | opam-ed 'get dev-repo' 
-done | sort -u | tr -d '"'
-
