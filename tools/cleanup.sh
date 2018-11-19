5f6fc117e910f2f1accf50df65d2bfc06968c3df
Christian Lindig <christian.lindig@citrix.com>
2018-11-19 10:57:15 +0000

    Add comment about purpose of cleanup.sh
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- tools/cleanup.sh
+++ tools/cleanup.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+#
+# list packages that are not required by packages in this repo
+#
 exec opam admin filter --dry-run --resolve=xs-toolstack --resolve upstream-extra-dummy --resolve depext.transition --resolve opam-ed.0.1 --or --with-test
