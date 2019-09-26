d707ed691f55c1187da8056879e40806c1ffe1d6
Pau Ruiz Safont <pau.safont@citrix.com>
2019-09-26 15:52:21 +0100

    CP-32055: remove unused packages
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- tools/cleanup.sh
+++ tools/cleanup.sh
@@ -2,4 +2,4 @@
 #
 # list packages that are not required by packages in this repo
 #
-exec opam admin filter --dry-run --resolve=xs-toolstack --resolve upstream-extra-dummy --resolve depext.transition --resolve opam-ed.0.3 --or --with-test
+exec opam admin filter --dry-run --resolve=xs-toolstack --resolve upstream-extra-dummy --resolve depext.transition --resolve opam-ed.0.3 --resolve ocaml-base-compiler.4.07.1 --resolve ocaml-system.4.07.1 --resolve ppx_tools.5.1+4.06.0 --resolve ocaml-system.4.08.1 --or --with-test
