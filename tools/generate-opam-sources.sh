167e102fb1d86e3d89274752dfb084700df0ed4e
Pau Ruiz Safont <pau.safont@citrix.com>
2021-09-20 13:44:01 +0100

    remove xapi-libs-transitional metapackage
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- tools/generate-opam-sources.sh
+++ tools/generate-opam-sources.sh
@@ -42,7 +42,6 @@ xapi-plugin               $XAPI/ocaml-xapi-plugin
 xapi-storage-script       $XAPI/xapi-storage-script
 xapi-storage-cli          $XAPI/sm-cli
 xapi-nbd                  $XAPI/xapi-nbd
-xapi-libs-transitional    $XAPI/xen-api-libs-transitional
 uuid                      $XAPI/xen-api-libs-transitional
 http-svr                  $XAPI/xen-api-libs-transitional
 safe-resources            $XAPI/xen-api-libs-transitional
