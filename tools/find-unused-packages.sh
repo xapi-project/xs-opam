6ffdaf74d2de345157f0366771f0fe755995defc
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2023-04-05 14:18:12 +0100

    ci: update setup-ocaml to v2
    This copies what is being used for the yangtze builds, which take about
    11 to 15 minutes compared to 21 to 27 when running with a cold cache and
    around 9 to 11 minutes with a warm cache
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- tools/find-unused-packages.sh
+++ tools/find-unused-packages.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+export OPAMCOLOR=NEVER
+
 USED=$(opam list -t --required-by xs-toolstack --recursive --short | sort)
 UPSTREAM=$(find "packages/upstream/" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} echo "{}" | cut -d "/" -f 3 | cut -d "." -f 1 | sort | uniq)
 XS=$(find "packages/xs/" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -I {} echo "{}" | cut -d "/" -f 3 | cut -d "." -f 1 | sort | uniq)
