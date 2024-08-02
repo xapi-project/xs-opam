f06bb3e6361c9ec709eeae09820b87b94de3c234
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2024-08-02 16:33:19 +0100

    tools: adapt print licenses to the new layout
    Now it uses the opam metadata as the source of truth. It now is able to
    stitch together lists of licenses to the SPDX format using AND as
    separator.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- tools/print-licenses.sh
+++ tools/print-licenses.sh
@@ -1,12 +1,27 @@
 #! /bin/bash
-# emit licenses for toolstack packages, needs to be run at the root of the
-# project 
+# Emit licenses for packaged dependencies
 
-set -e
+set -euo pipefail
 
-for file in packages/{upstream,xs}/*/opam;
+# Don't allow the CI settings to break the script
+OPAMCOLOR=NEVER
+
+# opam list filters are are to eager to filter packages out, so grep is needed
+# to remove unwanted packages by version. Use ; as separator and change IFS to
+# make sure splits are done by newlines
+SAVEDIFS=$IFS
+IFS=$'\n'
+for package in $(opam list --required-by xs-toolstack --recursive --short | sort | xargs opam info -f name:,license:,version: | paste -d ";" - - - | grep -ve 'version: "base"' -e 'version: "master"' -e '"ocaml"' -e '"ocaml-base-compiler"');
 do
-  name=$(basename ${file%%.*})
-  license=$(grep 'license: ' $file | awk -F ': ' '{print $2}')
-  echo $name: $license
+  IFS=$SAVEDIFS
+  name=$(echo "$package" | cut -f 1 -d ";" | cut -f 2 -d ":" | tr -d '"' | tr -d '[:space:]')
+  license_str=$(echo "$package" | cut -f 2 -d ";" | cut -f 2 -d ":")
+
+  licenses=()
+  while IFS= read -r -d '' license; do
+    licenses+=("$license");
+  done < <(echo "$license_str" | xargs printf '%s\0')
+
+  sep=" AND ";
+  ( IFS=; license_str="${licenses[*]/#/$sep}"; echo "$name": \""${license_str:${#sep}}"\")
 done
